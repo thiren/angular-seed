b9b857988d4a50a923db8efe91cefd1cc93cad31
Thiren Bunsee <thirenbunsee@gmail.com>
2016-01-20 23:06:18 +0200

    Added an sample unit test

---
+++ test/sample.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('angular-seed module', function() {
+
+    beforeEach(module('angular-seed'));
+
+    describe('header controller', function(){
+
+        it('should ....', inject(function($controller) {
+            //spec body
+            var view1Ctrl = $controller('headerController');
+            expect(view1Ctrl).toBeDefined();
+        }));
+
+    });
+});
