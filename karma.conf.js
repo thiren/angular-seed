3bf8b5aaa42208cd4fca842fa75f42790f0f2c0f
Thiren Bunsee <thirenbunsee@gmail.com>
2016-01-20 23:49:58 +0200

    updated the karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,11 @@
 module.exports = function(config){
     config.set({
 
+        frameworks: ['jasmine'],
+
+        // frameworks to use
+        //frameworks: ['mocha', 'sinon-chai'],
+
         basePath : './',
 
         files : [
@@ -11,12 +16,38 @@ module.exports = function(config){
             'bower_components/angular-mocks/angular-mocks.js',
             'bower_components/angular-resource/angular-resource.js',
             'bower_components/angular-ui-router/release/angular-ui-router.js',
-            'src/app/**/*.js'
+            'src/app/**/*.js',
+            'test/**/*.js'
         ],
 
+        // list of files to exclude
+        exclude: [],
+
+        // preprocess matching files before serving them to the browser
+        preprocessors: {
+            'src/*.js': ['coverage']
+        },
+
+        coverageReporter: {
+            type: 'text-summary',
+            dir: 'coverage/'
+        },
+
+        // test results reporter to use
+        reporters: ['progress', 'coverage'],
+
+        // web server port
+        port: 9876,
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+        // enable / disable watching file and executing tests on file changes
         autoWatch : true,
 
-        frameworks: ['jasmine'],
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
 
         browsers : ['Chrome'],
 
