02c774529b6e5437e750bccef7ca3ebe0ef22a4d
Thiren Bunsee <thirenbunsee@gmail.com>
2016-01-30 16:26:44 +0200

    Updated e2e tests

--- tests/e2e/senarios.js
+++ tests/e2e/senarios.js
@@ -6,25 +6,34 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 
-describe('my app', function() {
+describe('no protractor at all', function() {
+    it('should still do normal tests', function() {
+        expect(true).to.equal(true);
+    });
+});
 
-    it('should automatically redirect to /home when location is empty', function() {
+describe('protractor library', function() {
+    it.skip('should be able to skip tests', function() {
+        expect(true).to.equal(false);
+    });
+
+    it('should expose the correct global variables', function() {
+        expect(protractor).to.exist;
+        expect(browser).to.exist;
+        expect(by).to.exist;
+        expect(element).to.exist;
+        expect($).to.exist;
+    });
+
+    it('should be able to redirect', function() {
         browser.get('index.html');
         expect(browser.getLocationAbsUrl()).to.eventually.equal("/home");
     });
 
-
-    describe('home', function() {
-
-        beforeEach(function() {
-            browser.get('index.html/home');
-        });
-
-
-        it('should wrap webdriver', function() {
-            // Mocha will report the spec as slow if it goes over this time in ms.
-            this.slow(6000);
-            expect(browser.getTitle()).to.eventually.equal('Angular Seed');
-        });
+    it('should wrap webdriver', function() {
+        // Mocha will report the spec as slow if it goes over this time in ms.
+        this.slow(6000);
+        browser.get('index.html');
+        expect(browser.getTitle()).to.eventually.equal('Angular Seed');
     });
 });
