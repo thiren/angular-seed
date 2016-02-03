fe2e876821df872117c15b79ed9424fad6e75709
Thiren Bunsee <thirenbunsee@gmail.com>
2016-02-03 23:25:31 +0200

    Refactored the header module into its own file

--- src/app/header/header.controler.js
+++ src/app/header/header.controler.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
 
-    angular.module('angular-seed.header', [])
+    angular.module('angular-seed.header')
         .controller('HeaderController', controller);
 
     controller.$inject = ['$scope'];
