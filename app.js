88724e442cff541eeff53f2a00b22a2146965250
Thiren Bunsee <thiren.bunsee@entelect.co.za>
2016-02-04 16:24:16 +0200

    Created an express web server to host the app

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+var logger = require('morgan');
+var express = require('express');
+var compression = require('compression');
+var app = express();
+
+app.use(logger('dev'));
+app.use(compression());
+
+app.use(express.static(__dirname + '/build'));
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            status: 'error',
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        status: 'error',
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
