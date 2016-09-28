ca556cb1d8677c4ebd556ea4dcf3878a8301e3c1
Thiren Bunsee <thirenbunsee@gmail.com>
2016-09-28 21:20:58 +0200

    Added the console global to the eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
     "extends": "eslint:recommended",
+    "globals": {
+        "angular": true,
+        "console": true
+    },
     "rules": {
         "no-unused-vars": ["error", { "vars": "all", "args": "none" }]
-    },
-    "globals": {
-        "angular": true
     }
 };
